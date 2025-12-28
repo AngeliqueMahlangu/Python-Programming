c1531f6814ed86478003e64282401bd49e1d117c
AngeliqueMahlangu <168417263+AngeliqueMahlangu@users.noreply.github.com>
2025-12-28 18:17:43 +0200

    Add files via upload

---
+++ Madlibs_Game.py
@@ -0,0 +1,13 @@
+#Madlibs game, a word game
+
+adjective1= input("Enter an adjective: ")
+adjective2= input("Enter an adjective: ")
+adjective3= input("Enter an adjective: ")
+
+noun1= input("Enter a noun: ")
+
+
+print(f"Today i took a {adjective1} shower.")
+print(f"Then i went to the mall,where saw a {noun1}.")
+print(f"The {noun1} was {adjective2}.")
+print(f"I was too {adjective3} to speak.")
